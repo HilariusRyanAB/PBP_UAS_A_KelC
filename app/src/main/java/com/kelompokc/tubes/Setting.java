1580980aecf46b6cf3ba6f70a5e773cbb0a7c560
Yeremiayoga <yogayeremia@gmail.com>
2020-10-08 23:10:23 +0700

    night mode

---
+++ app/src/main/java/com/kelompokc/tubes/Setting.java
@@ -0,0 +1,47 @@
+package com.kelompokc.tubes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class Setting extends AppCompatActivity {
+    private Switch myswitch;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.darkTheme);
+        }
+        else{
+            setTheme(R.style.AppTheme);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        myswitch=(Switch)findViewById(R.id.myswitch);
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            myswitch.setChecked(true);
+        }
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
+            }
+        });
+    }
+    public void restartApp(){
+        Intent i = new Intent(getApplicationContext(),Setting.class);
+        startActivity(i);
+        finish();
+    }
+}
