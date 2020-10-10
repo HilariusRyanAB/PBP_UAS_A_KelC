0d3ed9fbdee6f89673763e7f7bdb9487346892f8
HilariusRyanAB <benedecash@gmail.com>
2020-10-10 18:14:49 +0700

    Tubes

--- app/src/main/java/com/kelompokc/tubes/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/kelompokc/tubes/ui/settings/SettingsFragment.java
@@ -4,24 +4,18 @@ import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
-import android.widget.SeekBar;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
@@ -35,95 +29,114 @@ import com.kelompokc.tubes.R;
 public class SettingsFragment extends Fragment
 {
     private String CHANNEL_ID="Channel 1";
-    MaterialButton logOutBtn;
-    private SwitchMaterial myswitch;
-    MaterialButton exitBtn;
-    MaterialButton tentangBtn;
-
     public static final String SHARE_PREFS = "SharedPrefs";
     public static final String SWITCH1 = "switch1";
 
+    private MaterialButton logOutBtn;
+    private SwitchMaterial myswitch;
+    private MaterialButton exitBtn;
+    private MaterialButton tentangBtn;
 
     public boolean switchOnOff;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
         myswitch = (SwitchMaterial) root.findViewById(R.id.myswitch);
-        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b) {
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b)
+            {
+                if(b)
+                {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                     getActivity().getApplicationContext().setTheme(R.style.AppTheme);
-                } else {
+                }
+                else
+                {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                     getActivity().getApplicationContext().setTheme(R.style.AppTheme);
+
                 }
                 saveData(b);
             }
         });
-        logOutBtn = root.findViewById(R.id.button_logOut);
-        logOutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new AlertDialog.Builder(getContext()).setTitle("Log Out").setMessage("Are You Sure?")
-                        .setPositiveButton("Log Out", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                FirebaseAuth.getInstance().signOut();
-                                createNotificationChannel();
-                                addNotification();
-                                startActivity(new Intent(getActivity().getApplicationContext(), LoginActivity.class));
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
 
-                            }
-                        })
-                        .create().show();
+        logOutBtn = root.findViewById(R.id.button_logOut);
+        logOutBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                logOutDialog();
             }
         });
 
         exitBtn = root.findViewById(R.id.button_exit);
-        exitBtn.setOnClickListener(new View.OnClickListener() {
+        exitBtn.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 getActivity().finish();
                 System.exit(0);
             }
         });
 
-
         tentangBtn = root.findViewById(R.id.button_tentang);
-        tentangBtn.setOnClickListener(new View.OnClickListener() {
+        tentangBtn.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                showDialog();
+            public void onClick(View view)
+            {
+                tentangDialog();
             }
         });
-
+        loadData();
         return root;
     }
 
-    private void showDialog() {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity().getApplicationContext());
-        alertDialogBuilder.setTitle("Atma Perpus");
-        alertDialogBuilder
-                .setMessage("Sebuah aplikasi yang akan membantu anda untuk mempermudah meminjam buku di perpustakan")
-                .setMessage("Atma Perpus dengan fitur yang memadai mempermudah peminjaman dan pengembalian buku")
-                .setMessage("dengan sistem yang mudah digunakan untuk siapa saja.")
-                .setCancelable(false)
-                .setNegativeButton("Back", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
+    public void logOutDialog()
+    {
+        new AlertDialog.Builder(getContext()).setTitle("Log Out").setMessage("Are You Sure?")
+                .setPositiveButton("Log Out", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i)
+                    {
+                        FirebaseAuth.getInstance().signOut();
+                        createNotificationChannel();
+                        addNotification();
+                        startActivity(new Intent(getActivity().getApplicationContext(), LoginActivity.class));
                     }
-                });
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i)
+                    {
 
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
+                    }
+                })
+                .create().show();
     }
+
+    public void tentangDialog()
+    {
+        new AlertDialog.Builder(getContext()).setTitle("Atma Perpus")
+                .setMessage("Sebuah aplikasi yang akan membantu anda untuk mempermudah meminjam buku di perpustakan " +
+                        "Atma Perpus dengan fitur yang memadai mempermudah peminjaman dan pengembalian buku "
+                        +"dengan sistem yang mudah digunakan untuk siapa saja.")
+                .setNegativeButton("Back", new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                    }
+                }).create().show();
+    }
+
     public void createNotificationChannel()
     {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
